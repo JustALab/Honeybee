c80610b0485ce494a06dbf23b86c427622649ab7
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-08-08 18:41:58 +0530

    back handler added

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import { BackHandler } from "react-native";
 import { SplashView } from "./src/Views/SplashView";
 import { createSwitchNavigator } from "react-navigation";
 import { Login } from "./src/Views/Login";
 import { MainView } from "./src/Views/MainView";
 
-export default (Routes = createSwitchNavigator(
+export default class App extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  handleBackButton() {
+    console.log("Back button is pressed");
+    return true;
+  }
+
+  render() {
+    return <AppRoutes />;
+  }
+}
+
+const AppRoutes = createSwitchNavigator(
   {
     splash: {
       screen: SplashView
@@ -19,4 +40,4 @@ export default (Routes = createSwitchNavigator(
     initialRouteName: "splash",
     headerMode: "none"
   }
-));
+);
