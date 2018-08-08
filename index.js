1bddb8f8779d914f4d8cbd6ee1ffefb996a4d3c8
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-08-08 16:25:44 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('justalab.honeycakes', () => App);
+AppRegistry.registerComponent('justalabhoneybee', () => App);
