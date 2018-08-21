2e2e8464fb48377344e3a09f86e07c253f140699
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-08-21 07:17:59 +0530

    header text color

--- src/Components/HeaderLab.js
+++ src/Components/HeaderLab.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Header, Left, Right, Body, Icon, Title} from 'native-base';
-import {ICONS} from '../Config/Icons';
-import {STRINGS} from '../Config/Strings';
-import { primary, secondary } from '../Config/Colors';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Header, Left, Right, Body, Icon, Title } from "native-base";
+import { ICONS } from "../Config/Icons";
+import { STRINGS } from "../Config/Strings";
+import { primary, secondary, onPrimary } from "../Config/Colors";
 
 export class HeaderLab extends React.Component {
-
-    render() {
-        
-        let headerLeftIcon;
-        if(this.props.leftButton === STRINGS.menu) {
-            headerLeftIcon = <Icon name={ICONS.menu} style={styles.headerIconLeft} />;
-        } else if(this.props.leftButton === STRINGS.close) {
-            headerLeftIcon = <Icon name={ICONS.close} style={styles.headerIconLeft} />;
-        } else if(this.props.leftButton === STRINGS.back) {
-            headerLeftIcon = <Icon name={ICONS.back} style={styles.headerIconLeft} />;
-        }
-
-        return (
-            <Header style={styles.header}>
-                <Left>
-                    {headerLeftIcon}
-                </Left>
-                <Body>
-                    <Title>{this.props.title}</Title>
-                </Body>
-                <Right />
-            </Header>
-        );
+  render() {
+    let headerLeftIcon;
+    if (this.props.leftButton === STRINGS.menu) {
+      headerLeftIcon = <Icon name={ICONS.menu} style={styles.headerIconLeft} />;
+    } else if (this.props.leftButton === STRINGS.close) {
+      headerLeftIcon = (
+        <Icon name={ICONS.close} style={styles.headerIconLeft} />
+      );
+    } else if (this.props.leftButton === STRINGS.back) {
+      headerLeftIcon = <Icon name={ICONS.back} style={styles.headerIconLeft} />;
     }
 
+    return (
+      <Header style={styles.header}>
+        <Left>{headerLeftIcon}</Left>
+        <Body>
+          <Title style={styles.headerText}>{this.props.title}</Title>
+        </Body>
+        <Right />
+      </Header>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    headerIconLeft: {
-        paddingLeft: 2,
-        color: secondary
-    }, 
-    header: {
-        backgroundColor: primary
-    }
+  headerIconLeft: {
+    paddingLeft: 2,
+    color: secondary
+  },
+  header: {
+    backgroundColor: primary
+  },
+  headerText: {
+    color: onPrimary
+  }
 });
